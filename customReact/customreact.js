c74f0fd4d593b147aa2279c7c387e58c2a430bd4
Yashraj Agarwal <yashrajagarwal81@gmail.com>
2024-11-08 19:41:16 +0530

    commit 2

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -7,9 +7,17 @@ function customRender(reactElement,mainContainer){
 
     mainContainer.appendChild(domElement)
     */
-   
-}
 
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop === children) continue;
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
+    mainContainer.appendChild(domElement)
+}
+        
 const reactElement = {
     type:'a',
     props:{
